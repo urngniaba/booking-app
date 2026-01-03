eec50668c4ae3c57d926ebd0437acb7a9d3f8b1b
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2026-01-03 00:46:02 -0500

    Appointment  api POST

--- app/api/appointments/route.ts
+++ app/api/appointments/route.ts
@@ -1,45 +1,29 @@
-// import { NextResponse } from "next/server";
-// import prisma from "@/lib/prisma";
-// import { createAppointmentSchema } from "@/schemas/appointment.schema";
+import { NextResponse } from "next/server";
+import { createAppointment } from "@/services/appointment.service";
+import { AppointmentSchemaConstraints } from "@/schemas/appointment.schema";
 
-// export async function POST(req: Request) {
-//   const body = await req.json();
-//   const { timeSlotId } = createAppointmentSchema.parse(body);
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const parsedAppointment = AppointmentSchemaConstraints.safeParse(body);
 
-//   const userId = req.headers.get("x-user-id")!;
-
-//   // 1 rendez-vous max
-//   const existing = await prisma.appointment.findFirst({
-//     where: { clientId: userId, status: "PENDING" },
-//   });
-
-//   if (existing) {
-//     return NextResponse.json(
-//       { error: "Un rendez-vous est déjà en cours" },
-//       { status: 400 }
-//     );
-//   }
-
-//   const slot = await prisma.timeSlot.findUnique({ where: { id: timeSlotId } });
-//   if (!slot || !slot.isAvailable) {
-//     return NextResponse.json(
-//       { error: "Créneau indisponible" },
-//       { status: 400 }
-//     );
-//   }
-
-//   const appointment = await prisma.appointment.create({
-//     data: {
-//       clientId: userId,
-//       timeSlotId,
-//     },
-//   });
-
-//   await prisma.timeSlot.update({
-//     where: { id: timeSlotId },
-//     data: { isAvailable: false },
-//   });
-
-//   return NextResponse.json(appointment);
-// }
+    if (!parsedAppointment.success) {
+      return NextResponse.json(
+        { error: "Invalid appointment data", details: parsedAppointment.error },
+        { status: 400 }
+      );
+    }
+    const newAppointment = await createAppointment(
+      parsedAppointment.data.clientId,
+      parsedAppointment.data.timeSlotId
+    );
+    return NextResponse.json(newAppointment, { status: 201 });
+    
+    } catch (error) {
+        return NextResponse.json(
+        { error: "Failed to process request", details: error },
+        { status: 500 }
+      );
+    }
+}
 
